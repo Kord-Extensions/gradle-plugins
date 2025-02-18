fcf6b87cefb8f130b55e4d91ca1171cd4dfa52ca
Gareth Coles <gdude2002@gmail.com>
2025-02-18 21:14:00 +0000

    Fix test module

--- testModule/settings.gradle.kts
+++ testModule/settings.gradle.kts
@@ -2,7 +2,7 @@ pluginManagement {
 	plugins {
 		val pluginVersion = "1.6.1"
 
-		kotlin("jvm") version "2.0.21"
+		kotlin("jvm") version "2.1.0"
 
 		id("com.google.devtools.ksp") version "2.0.20-1.0.24"
 		id("dev.kordex.gradle.docker") version pluginVersion
