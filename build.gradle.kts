baca29719b12a3075f9cc6c95700cd13c6a24943
Gareth Coles <gareth@gareth-coles.dev>
2025-08-14 17:35:20 +0100

    SonarQube

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,34 @@
+plugins {
+	id("org.sonarqube")
+}
+
 val projectVersion: String by project
 
 group = "dev.kordex.gradle.plugins"
 version = projectVersion
+
+sonar {
+	val org = "Kord-Extensions"
+	val gitUrl = "https://github.com/${org}/gradle-plugins/"
+	val homepageUrl = "https://kordex.dev"
+
+	properties {
+		property("sonar.sourceEncoding", "UTF-8")
+		property("sonar.projectName", "gradle-plugins")
+		property("sonar.projectKey", "${org}_gradle-plugins")
+		property("sonar.organization", "Kord-Extensions")
+		property("sonar.projectVersion", rootProject.version.toString())
+		property("sonar.host.url", System.getenv()["SONAR_HOST_URL"] ?: "")
+		property("sonar.token", System.getenv()["SONAR_TOKEN"] ?: "" )
+		property("sonar.scm.provider", "git")
+		property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/kover/report.xml")
+
+		property("sonar.links.homepage", homepageUrl)
+		property("sonar.links.ci", "$gitUrl/actions")
+		property("sonar.links.scm", gitUrl)
+		property("sonar.links.issue", "$gitUrl/issues")
+	}
+}
 
 subprojects {
 	this.group = "dev.kordex.gradle.plugins"
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
 
 	implementation("dev.yumi", "yumi-gradle-licenser", "1.2.0")
 	implementation("io.gitlab.arturbosch.detekt", "detekt-gradle-plugin", "1.23.6")
+	implementation("org.sonarqube:org.sonarqube.gradle.plugin:6.2.0.5505")
 
 	implementation(gradleApi())
 	implementation(localGroovy())
