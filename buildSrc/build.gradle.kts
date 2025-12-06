3703b2229cd63779a6b77c5f757d783ac0bd513e
Gareth Coles <gareth@gareth-coles.dev>
2025-12-06 16:04:16 +0000

    Update Sonar plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
 
 	implementation("dev.yumi", "yumi-gradle-licenser", "1.2.0")
 	implementation("io.gitlab.arturbosch.detekt", "detekt-gradle-plugin", "1.23.8")
-	implementation("org.sonarqube:org.sonarqube.gradle.plugin:6.2.0.5505")
+	implementation("org.sonarqube:org.sonarqube.gradle.plugin:7.1.0.6387")
 	implementation("org.jetbrains.kotlinx.kover:org.jetbrains.kotlinx.kover.gradle.plugin:0.9.3")
 
 	implementation(gradleApi())
